326730c6ef1a78d46284753152abfaf4c64f3443
Robert Obi <robobi97@googlemail.com>
2020-12-31 07:02:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,8 +87,10 @@ def adding_matz
       }
     }
 
+programmer_hash[:yukihiro_matsumoto] = [ {:known_for => "Ruby"}, {:languages => ["LISP", "C"]
+}]
 
-  
+  epic_tragedy[:additional_characters] = [ {name: "Prince Escalus"}, {name: "Apothecary"} ]
 
 
 

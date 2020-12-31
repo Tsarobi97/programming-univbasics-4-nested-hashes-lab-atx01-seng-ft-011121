68d65a6300b0ee0c1a18b6dd7d28f89c745ff02e
Robert Obi <robobi97@googlemail.com>
2020-12-31 07:29:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -111,8 +111,10 @@ def changing_alan
       }
     }
 alans_new_info = "GUI"
-    programmer_hash[:alan_kay][:known_for] = alans_new_info
-    programmer_hash
+programmer_hash[:alan_kay][:known_for] = alans_new_info
+programmer_hash
+
+
 
 end
 

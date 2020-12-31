80e5455ed53aeef90f9ede8b6e6657a04b73547f
Robert Obi <robobi97@googlemail.com>
2020-12-31 06:43:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,9 +86,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-    
-    adding_matz = { :yukihiro_matsumoto => {:known_for => "Ruby"}, {:languages => ["LISP", "C"] }
-puts adding_matz
+    programmer_hash[:yukihiro_matsumoto] = 
  
 end
 

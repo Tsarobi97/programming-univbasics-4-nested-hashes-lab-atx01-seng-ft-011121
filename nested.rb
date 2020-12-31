3e0d62e74a6dd7f9b7f0ef3458f7e0a0ec73e708
Robert Obi <robobi97@googlemail.com>
2020-12-31 06:02:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-        programmer_hash[:dennis_ritchies][:languages][0]
+        programmer_hash[:dennis_ritchies][:languages][1]
 
 
 end
